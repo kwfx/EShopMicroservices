36dc313ace6d60ddd08c3142b8ad9cc8c2b216cc
labgo-fahmi <6350273@cgodin.qc.ca>
2025-11-11 19:48:05 -0500

    Create buildingBlocks projects Create ICommand and IQuery abstractions for CQRS

---
+++ src/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+
+    }
+
+    public interface ICommand : ICommand<Unit>
+    {
+
+    }
+}
