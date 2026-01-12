53dcba8fe9fcc9b2a9e03e449adbdff8d780eaf3
kwfx <kw.fahmi@gmail.com>
2026-01-12 18:43:46 -0500

    Add ClassLib BuildingBlocks.Messaging

---
+++ src/BuildingBlocks.Messaging/MassTransit/MessageBrokerExtension.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class MessageBrokerExtension
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly)
+    {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+            if (assembly is not null)
+            {
+                cfg.AddConsumers(assembly);
+            }
+            cfg.UsingRabbitMq((context, rabbitMQConfig) =>
+            {
+                rabbitMQConfig.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:Username"]!);
+                    host.Password(configuration["MessageBroker:password"]!);
+                });
+                rabbitMQConfig.ConfigureEndpoints(context);
+            });
+        });
+        return services;
+    }
+}
